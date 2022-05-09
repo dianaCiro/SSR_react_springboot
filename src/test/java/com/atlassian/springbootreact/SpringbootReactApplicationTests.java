490b750f2323d012c772f3893ef0d6448686569b
dianaciro <kath9463@gmail.com>
2022-05-09 18:16:06 -0500

    initial configuration to server side rendering react with spring boot and maven

---
+++ src/test/java/com/atlassian/springbootreact/SpringbootReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atlassian.springbootreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
